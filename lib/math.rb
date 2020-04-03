ef0d1cdb16bb40542d62f97486c88b89e3e02558
Philip Roush <roush.philip@gmail.com>
2020-04-03 23:45:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,15 +2,19 @@ def addition(num1 = 68, num2 = 1)
     phil = num1 + num2
     return phil
 end
-def subtraction(num1, num2)
-
+def subtraction(num1 = 100, num2 = 31)
+    roush = num1 - num2
+    return roush
 end
 
-def division(num1, num2)
-
+def division(num1 = 100, num2 = 10)
+    ass = num1 / num2
+    return ass
 end
 
-def multiplication(num1, num2)
+def multiplication(num1= 6, num2= 9)
+  nixtysine = num1 * num2
+  return nixtysine
 
 end
 
